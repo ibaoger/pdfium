d46175f440eec6d943ed17ac474217d5d017dd1e
Dan Sinclair <dsinclair@chromium.org>
2017-01-23 16:06:13 -0500

    Track CFDE_CSSComputedStyle with retained ptrs
    Remove the bare new and use CFX_RetainPtr to keep track of the computed styles.
    
    Change-Id: Icf235623529797176707482c78676814b7a81b9e

--- xfa/fde/css/cfde_csscomputedstyle.h
+++ xfa/fde/css/cfde_csscomputedstyle.h
@@ -15,7 +15,7 @@
 
 class CFDE_CSSValueList;
 
-class CFDE_CSSComputedStyle : public IFX_Retainable {
+class CFDE_CSSComputedStyle : public CFX_Retainable {
  public:
   class InheritedData {
    public:
@@ -56,10 +56,6 @@ class CFDE_CSSComputedStyle : public IFX_Retainable {
 
   CFDE_CSSComputedStyle();
   ~CFDE_CSSComputedStyle() override;
-
-  // IFX_Retainable
-  uint32_t Retain() override;
-  uint32_t Release() override;
 
   int32_t CountFontFamilies() const;
   const CFX_WideString GetFontFamily(int32_t index) const;
@@ -105,7 +101,6 @@ class CFDE_CSSComputedStyle : public IFX_Retainable {
   NonInheritedData m_NonInheritedData;
 
  private:
-  uint32_t m_dwRefCount;
   std::vector<CFX_WideString> m_CustomProperties;
 };
 
