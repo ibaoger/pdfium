e1c5f983ffd4afdc8d58ff502a1815f42250bc8c
Lei Zhang <thestig@chromium.org>
2017-02-24 14:07:17 -0800

    Remove some pointless checks and assignments.
    Change-Id: I6f9fd67cdac581e8f91f829a5934b763bb229d21

--- core/fpdfapi/parser/cpdf_data_avail.cpp
+++ core/fpdfapi/parser/cpdf_data_avail.cpp
@@ -1123,13 +1123,8 @@ bool CPDF_DataAvail::CheckArrayPageNode(uint32_t dwPageNo,
     return false;
   }
 
-  if (!pPages) {
-    if (m_docStatus == PDF_DATAAVAIL_ERROR) {
-      m_docStatus = PDF_DATAAVAIL_ERROR;
-      return false;
-    }
+  if (!pPages)
     return false;
-  }
 
   CPDF_Array* pArray = pPages->AsArray();
   if (!pArray) {
@@ -1160,11 +1155,8 @@ bool CPDF_DataAvail::CheckUnknownPageNode(uint32_t dwPageNo,
     return false;
   }
 
-  if (!pPage) {
-    if (m_docStatus == PDF_DATAAVAIL_ERROR)
-      m_docStatus = PDF_DATAAVAIL_ERROR;
+  if (!pPage)
     return false;
-  }
 
   if (pPage->IsArray()) {
     pPageNode->m_dwPageNo = dwPageNo;
