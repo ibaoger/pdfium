f7341f631dc3c8428db90dd0a6537cca9cb20dad
Tom Sepez <tsepez@chromium.org>
2017-03-02 12:00:01 -0800

    Use std::deque for CFX_MassArrayTemplate<FDE_TEXTEDITPIECE>
    Change-Id: I1080eefb4e47d7bc86fb3384fd7479a1fd49b203

--- xfa/fde/fde_render.cpp
+++ xfa/fde/fde_render.cpp
@@ -61,7 +61,6 @@ FDE_RENDERSTATUS CFDE_RenderContext::DoRender(IFX_Pause* pPause) {
   rm.TransformRect(rtDocClip);
   IFDE_VisualSet* pVisualSet;
   FDE_TEXTEDITPIECE* pPiece;
-  CFX_RectF rtObj;
   int32_t iCount = 0;
   while (true) {
     pPiece = m_pIterator->GetNext(pVisualSet);
@@ -69,8 +68,7 @@ FDE_RENDERSTATUS CFDE_RenderContext::DoRender(IFX_Pause* pPause) {
       eStatus = FDE_RENDERSTATUS_Done;
       break;
     }
-    rtObj.Empty();
-    pVisualSet->GetRect(pPiece, rtObj);
+    CFX_RectF rtObj = pVisualSet->GetRect(*pPiece);
     if (!rtDocClip.IntersectWith(rtObj))
       continue;
 
@@ -111,7 +109,7 @@ void CFDE_RenderContext::RenderText(IFDE_TextSet* pTextSet,
   if (!pFont)
     return;
 
-  int32_t iCount = pTextSet->GetDisplayPos(pText, nullptr, false);
+  int32_t iCount = pTextSet->GetDisplayPos(*pText, nullptr, false);
   if (iCount < 1)
     return;
 
@@ -121,7 +119,7 @@ void CFDE_RenderContext::RenderText(IFDE_TextSet* pTextSet,
   if (m_CharPos.size() < static_cast<size_t>(iCount))
     m_CharPos.resize(iCount, FXTEXT_CHARPOS());
 
-  iCount = pTextSet->GetDisplayPos(pText, m_CharPos.data(), false);
+  iCount = pTextSet->GetDisplayPos(*pText, m_CharPos.data(), false);
   FX_FLOAT fFontSize = pTextSet->GetFontSize();
   FX_ARGB dwColor = pTextSet->GetFontColor();
   m_pBrush->SetColor(dwColor);
