7cc8bf44d16a2ec135a4cd3e110aa9b3ba7b11ca
Dan Sinclair <dsinclair@chromium.org>
2017-03-14 15:02:31 -0400

    Replace CFDE_TxtEdtPage FX_POSITION usage with uint32_t
    Change-Id: I8cd68aaeb3c1f7ba92f32bc4846bf2e7d02111e4

--- xfa/fde/cfde_txtedtpage.h
+++ xfa/fde/cfde_txtedtpage.h
@@ -46,8 +46,8 @@ class CFDE_TxtEdtPage : public IFDE_VisualSet {
   void UnloadPage(const CFX_RectF* pClipBox);
   const CFX_RectF& GetContentsBox();
 
-  FX_POSITION GetFirstPosition();
-  FDE_TEXTEDITPIECE* GetNext(FX_POSITION& pos, IFDE_VisualSet*& pVisualSet);
+  uint32_t GetFirstPosition();
+  FDE_TEXTEDITPIECE* GetNext(uint32_t* pos, IFDE_VisualSet*& pVisualSet);
 
   wchar_t GetChar(const FDE_TEXTEDITPIECE* pIdentity, int32_t index) const;
   int32_t GetWidth(const FDE_TEXTEDITPIECE* pIdentity, int32_t index) const;
