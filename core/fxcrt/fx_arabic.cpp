d9602924bb794be892e97665dd8a7ddfd1073032
Tom Sepez <tsepez@chromium.org>
2017-03-15 09:25:57 -0700

    Fix bounds error introduced at 193e6ca.
    Change-Id: I8e47124ae2a8e0a1bef7c15e3282fcc79cbd8c54

--- core/fxcrt/fx_arabic.cpp
+++ core/fxcrt/fx_arabic.cpp
@@ -416,8 +416,8 @@ class CFX_BidiLineTemplate {
                             int32_t iStart,
                             int32_t iCount) {
     ASSERT(pdfium::IndexInBounds(chars, iStart));
-    ASSERT(pdfium::IndexInBounds(chars, iCount));
-    ASSERT(iStart + iCount <= pdfium::CollectionSize<int32_t>(chars));
+    ASSERT(iCount >= 0 &&
+           iStart + iCount <= pdfium::CollectionSize<int32_t>(chars));
     std::reverse(chars.begin() + iStart, chars.begin() + iStart + iCount);
   }
 
@@ -426,7 +426,7 @@ class CFX_BidiLineTemplate {
                              int32_t iStart,
                              int32_t iCount,
                              int32_t iValue) {
-    ASSERT(pdfium::IndexInBounds(chars, iStart));
+    ASSERT(iStart >= 0 && iStart <= pdfium::CollectionSize<int32_t>(chars));
     ASSERT(iStart - iCount > -1);
     int32_t iLast = iStart - iCount;
     if (bClass) {
