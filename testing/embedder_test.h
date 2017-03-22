69d7db182447402dde5a92e03de0aca0574d2849
Nicolas Pena <npm@chromium.org>
2017-03-22 10:58:32 -0400

    Fix Mac tests on 10.12
    md5's with text are changed, so need to allow both the previous and the new one.
    Pixel tests with text also fail on 10.12, so suppressing those.
    Found many unexpected successes, so will unsuppress those once all bots are 10.12
    
    BUG=chromium:703912
    
    Change-Id: I6b34aa2f581eb8ea705f3876960b49c89e249347

--- testing/embedder_test.h
+++ testing/embedder_test.h
@@ -111,6 +111,14 @@ class EmbedderTest : public ::testing::Test,
                             int expected_height,
                             const char* expected_md5sum);
 
+  // Check |bitmap| to make sure it has the right dimensions and content.
+  // Have two options for the expected md5 sum.
+  static void CompareBitmap2Options(FPDF_BITMAP bitmap,
+                                    int expected_width,
+                                    int expected_height,
+                                    const char* option1_md5sum,
+                                    const char* option2_md5sum);
+
  protected:
   void SetupFormFillEnvironment();
 
