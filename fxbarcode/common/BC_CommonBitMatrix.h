f65b9fde9d3f0a22e0fe35527d06162d5a837661
Lei Zhang <thestig@chromium.org>
2017-04-04 17:01:37 -0700

    Change some fxbarcode to use return values.
    No caller cares about the exception values anyway. Remove the unused
    ones. Also use more std::unique_ptr to stop potential leaks.
    
    Change-Id: Ic5955fb0d879f55e1c6a005c0204df50246dab19

--- fxbarcode/common/BC_CommonBitMatrix.h
+++ fxbarcode/common/BC_CommonBitMatrix.h
@@ -14,20 +14,16 @@ class CBC_CommonBitArray;
 class CBC_CommonBitMatrix {
  public:
   CBC_CommonBitMatrix();
-  virtual ~CBC_CommonBitMatrix();
+  ~CBC_CommonBitMatrix();
 
-  virtual void Init(int32_t dimension);
-  virtual void Init(int32_t width, int32_t height);
+  void Init(int32_t dimension);
+  void Init(int32_t width, int32_t height);
 
   bool Get(int32_t x, int32_t y);
   void Set(int32_t x, int32_t y);
   void Flip(int32_t x, int32_t y);
   void Clear();
-  void SetRegion(int32_t left,
-                 int32_t top,
-                 int32_t width,
-                 int32_t height,
-                 int32_t& e);
+  bool SetRegion(int32_t left, int32_t top, int32_t width, int32_t height);
   CBC_CommonBitArray* GetRow(int32_t y, CBC_CommonBitArray* row);
   void SetRow(int32_t y, CBC_CommonBitArray* row);
   CBC_CommonBitArray* GetCol(int32_t y, CBC_CommonBitArray* row);
@@ -35,7 +31,6 @@ class CBC_CommonBitMatrix {
   int32_t GetWidth();
   int32_t GetHeight();
   int32_t GetRowSize();
-  int32_t GetDimension(int32_t& e);
   int32_t* GetBits();
 
  private:
