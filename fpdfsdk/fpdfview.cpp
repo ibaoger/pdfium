2daf4c107d59cc0e6ae6f46ffd9245b4fadabe9a
dan sinclair <dsinclair@chromium.org>
2017-04-19 23:17:52 -0400

    Experimental C++ API
    This Cl adds the basis of the experimental C++ API. This code is experimental
    and should not be used yet.
    
    BUG=pdfium:612
    
    Change-Id: I3cfddba7d61eadb2a79320ffaeb12c45c71f9894

--- fpdfsdk/fpdfview.cpp
+++ fpdfsdk/fpdfview.cpp
@@ -49,22 +49,6 @@
 #include "public/fpdf_formfill.h"
 #endif  // PDF_ENABLE_XFA
 
-#ifdef PDF_ENABLE_XFA_BMP
-#include "core/fxcodec/codec/ccodec_bmpmodule.h"
-#endif
-
-#ifdef PDF_ENABLE_XFA_GIF
-#include "core/fxcodec/codec/ccodec_gifmodule.h"
-#endif
-
-#ifdef PDF_ENABLE_XFA_PNG
-#include "core/fxcodec/codec/ccodec_pngmodule.h"
-#endif
-
-#ifdef PDF_ENABLE_XFA_TIFF
-#include "core/fxcodec/codec/ccodec_tiffmodule.h"
-#endif
-
 #if _FXM_PLATFORM_ == _FXM_PLATFORM_WINDOWS_
 #include "core/fxge/cfx_windowsdevice.h"
 #endif
@@ -381,30 +365,8 @@ FPDF_InitLibraryWithConfig(const FPDF_LIBRARY_CONFIG* cfg) {
   CPDF_ModuleMgr* pModuleMgr = CPDF_ModuleMgr::Get();
   pModuleMgr->SetCodecModule(g_pCodecModule);
   pModuleMgr->InitPageModule();
-  pModuleMgr->LoadEmbeddedGB1CMaps();
-  pModuleMgr->LoadEmbeddedJapan1CMaps();
-  pModuleMgr->LoadEmbeddedCNS1CMaps();
-  pModuleMgr->LoadEmbeddedKorea1CMaps();
-
-#ifdef PDF_ENABLE_XFA_BMP
-  pModuleMgr->GetCodecModule()->SetBmpModule(
-      pdfium::MakeUnique<CCodec_BmpModule>());
-#endif
-
-#ifdef PDF_ENABLE_XFA_GIF
-  pModuleMgr->GetCodecModule()->SetGifModule(
-      pdfium::MakeUnique<CCodec_GifModule>());
-#endif
-
-#ifdef PDF_ENABLE_XFA_PNG
-  pModuleMgr->GetCodecModule()->SetPngModule(
-      pdfium::MakeUnique<CCodec_PngModule>());
-#endif
-
-#ifdef PDF_ENABLE_XFA_TIFF
-  pModuleMgr->GetCodecModule()->SetTiffModule(
-      pdfium::MakeUnique<CCodec_TiffModule>());
-#endif
+  pModuleMgr->LoadEmbeddedMaps();
+  pModuleMgr->LoadCodecModules();
 
 #ifdef PDF_ENABLE_XFA
   FXJSE_Initialize();
---
+++ public/exp/pdfium.h
@@ -0,0 +1,28 @@
+// Copyright 2017 PDFium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef PUBLIC_EXP_PDFIUM_H_
+#define PUBLIC_EXP_PDFIUM_H_
+
+#include <memory>
+
+// NOLINTNEXTLINE(build/include)
+#include "pdfium_config.h"
+// NOLINTNEXTLINE(build/include)
+#include "pdfium_status.h"
+// NOLINTNEXTLINE(build/include)
+#include "pdfium_unsupported_feature_delegate.h"
+
+// This file is the main include for the PDFium API.
+
+namespace pdfium {
+
+void Initialize(std::unique_ptr<PDFiumConfig> cfg);
+void SetUnsupportedFeatureDelegate(
+    std::unique_ptr<UnsupportedFeatureDelegate> delgate);
+void Shutdown();
+
+}  // namespace pdfium
+
+#endif  // PUBLIC_EXP_PDFIUM_H_
