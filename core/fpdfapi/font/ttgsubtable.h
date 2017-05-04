25320a8feb4ce441e9a004e44f864d25f3e90c9e
Tom Sepez <tsepez@chromium.org>
2017-05-04 10:18:37 -0700

    Remove CFX_GlyphMap
    It's only instantiated in one place, and then nothing is ever
    put into the map.
    
    Change-Id: I8ebee324e0ec63c139309275c8de115d38063dc6

--- core/fpdfapi/font/ttgsubtable.h
+++ core/fpdfapi/font/ttgsubtable.h
@@ -11,23 +11,12 @@
 
 #include <memory>
 #include <set>
+#include <utility>
 #include <vector>
 
 #include "core/fxcrt/fx_basic.h"
 #include "core/fxge/fx_font.h"
 #include "core/fxge/fx_freetype.h"
-
-class CFX_GlyphMap {
- public:
-  CFX_GlyphMap();
-  ~CFX_GlyphMap();
-
-  void SetAt(int key, int value);
-  bool Lookup(int key, int& value);
-
- protected:
-  CFX_BinaryBuf m_Buffer;
-};
 
 class CFX_CTTGSUBTable {
  public:
@@ -154,7 +143,6 @@ class CFX_CTTGSUBTable {
     virtual ~TCoverageFormatBase() {}
 
     uint16_t CoverageFormat;
-    CFX_GlyphMap m_glyphMap;
 
    private:
     TCoverageFormatBase(const TCoverageFormatBase&);
