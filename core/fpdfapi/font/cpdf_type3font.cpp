898dd749a8af4f6bd006e9bb4fc5751cf12f92f6
Tom Sepez <tsepez@chromium.org>
2017-05-18 15:26:10 -0700

    Add next batch of CFX_UnownedPtr conversions.
    Too many ifdefs.
    Fix some issues with equality operators and type conversions.
    Provide a Release method to use with manaul deletion.
    
    Change-Id: I010e694c1649a2e6ffc9fabea421c72a3f20731c

--- core/fpdfapi/font/cpdf_type3font.cpp
+++ core/fpdfapi/font/cpdf_type3font.cpp
@@ -107,8 +107,9 @@ CPDF_Type3Char* CPDF_Type3Font::LoadChar(uint32_t charcode) {
 
   auto pNewChar =
       pdfium::MakeUnique<CPDF_Type3Char>(pdfium::MakeUnique<CPDF_Form>(
-          m_pDocument, m_pFontResources ? m_pFontResources : m_pPageResources,
-          pStream, nullptr));
+          m_pDocument.Get(),
+          m_pFontResources ? m_pFontResources : m_pPageResources, pStream,
+          nullptr));
 
   // This can trigger recursion into this method. The content of |m_CacheMap|
   // can change as a result. Thus after it returns, check the cache again for
