f2005521ddbaa701ab21cae7c1190138d1c69c56
Dan Sinclair <dsinclair@chromium.org>
2017-05-30 16:44:37 -0400

    Start adding CFGAS_FormatString tests
    This Cl adds tests for the date parsing and formatting code in
    CFGAS_FormatString.
    
    Change-Id: I87f1e3929226db1dcb2449736b089ef886fff668

--- xfa/fgas/crt/cfgas_formatstring.cpp
+++ xfa/fgas/crt/cfgas_formatstring.cpp
@@ -1989,7 +1989,6 @@ bool CFGAS_FormatString::ParseDateTime(const CFX_WideString& wsSrcDateTime,
                                        FX_DATETIMETYPE eDateTimeType,
                                        CFX_DateTime* dtValue) {
   dtValue->Reset();
-
   if (wsSrcDateTime.IsEmpty() || wsPattern.IsEmpty())
     return false;
 
@@ -2597,7 +2596,8 @@ bool CFGAS_FormatString::FormatDateTime(const CFX_WideString& wsSrcDateTime,
   if (wsSrcDateTime.IsEmpty() || wsPattern.IsEmpty())
     return false;
 
-  CFX_WideString wsDatePattern, wsTimePattern;
+  CFX_WideString wsDatePattern;
+  CFX_WideString wsTimePattern;
   IFX_Locale* pLocale = nullptr;
   FX_DATETIMETYPE eCategory =
       GetDateTimeFormat(wsPattern, pLocale, wsDatePattern, wsTimePattern);
