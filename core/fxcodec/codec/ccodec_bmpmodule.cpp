9df3bfdb93529d67cc67050590b3a9d3c27ce059
Tom Sepez <tsepez@chromium.org>
2017-05-31 13:10:23 -0700

    Move all of ccodec_bmpmodule state to its context
    This avoids a stale delegate pointer issue in the module. In theory,
    it should also allow for multiple decodes at the same time from 
    different contexts within the same module, but this isn't used.
    
    Bug: 728323
    Change-Id: I7bb66bb5d5b4fa173bec2b445a8e71ab691fdf5c

--- core/fxcodec/codec/ccodec_bmpmodule.cpp
+++ core/fxcodec/codec/ccodec_bmpmodule.cpp
@@ -9,15 +9,18 @@
 #include "core/fxcodec/codec/codec_int.h"
 #include "core/fxcodec/fx_codec.h"
 #include "core/fxcodec/lbmp/fx_bmp.h"
+#include "core/fxcrt/cfx_unowned_ptr.h"
 #include "core/fxge/fx_dib.h"
 
 struct FXBMP_Context {
   bmp_decompress_struct_p bmp_ptr;
-  void* parent_ptr;
-
+  CCodec_BmpModule* parent_ptr;
+  CFX_UnownedPtr<CCodec_BmpModule::Delegate> delegate_ptr;
   void* (*m_AllocFunc)(unsigned int);
   void (*m_FreeFunc)(void*);
+  char m_szLastError[256];
 };
+
 extern "C" {
 static void* bmp_alloc_func(unsigned int size) {
   return FX_Alloc(char, size);
@@ -25,52 +28,46 @@ static void* bmp_alloc_func(unsigned int size) {
 static void bmp_free_func(void* p) {
   FX_Free(p);
 }
-};
+}  // extern "C"
+
 static void bmp_error_data(bmp_decompress_struct_p bmp_ptr,
                            const char* err_msg) {
   strncpy((char*)bmp_ptr->err_ptr, err_msg, BMP_MAX_ERROR_SIZE - 1);
   longjmp(bmp_ptr->jmpbuf, 1);
 }
+
 static void bmp_read_scanline(bmp_decompress_struct_p bmp_ptr,
                               int32_t row_num,
                               uint8_t* row_buf) {
-  FXBMP_Context* p = (FXBMP_Context*)bmp_ptr->context_ptr;
-  CCodec_BmpModule* pModule = (CCodec_BmpModule*)p->parent_ptr;
-  pModule->GetDelegate()->BmpReadScanline(row_num, row_buf);
-}
-static bool bmp_get_data_position(bmp_decompress_struct_p bmp_ptr,
-                                  uint32_t rcd_pos) {
-  FXBMP_Context* p = (FXBMP_Context*)bmp_ptr->context_ptr;
-  CCodec_BmpModule* pModule = (CCodec_BmpModule*)p->parent_ptr;
-  return pModule->GetDelegate()->BmpInputImagePositionBuf(rcd_pos);
+  auto* p = reinterpret_cast<FXBMP_Context*>(bmp_ptr->context_ptr);
+  p->delegate_ptr->BmpReadScanline(row_num, row_buf);
 }
 
-CCodec_BmpModule::CCodec_BmpModule() {
-  memset(m_szLastError, 0, sizeof(m_szLastError));
+static bool bmp_get_data_position(bmp_decompress_struct_p bmp_ptr,
+                                  uint32_t rcd_pos) {
+  auto* p = reinterpret_cast<FXBMP_Context*>(bmp_ptr->context_ptr);
+  return p->delegate_ptr->BmpInputImagePositionBuf(rcd_pos);
 }
+
+CCodec_BmpModule::CCodec_BmpModule() {}
 
 CCodec_BmpModule::~CCodec_BmpModule() {}
 
-FXBMP_Context* CCodec_BmpModule::Start() {
-  FXBMP_Context* p = FX_Alloc(FXBMP_Context, 1);
-  if (!p)
+FXBMP_Context* CCodec_BmpModule::Start(Delegate* pDelegate) {
+  bmp_decompress_struct_p pDecomp = bmp_create_decompress();
+  if (pDecomp)
     return nullptr;
 
-  memset(p, 0, sizeof(FXBMP_Context));
-  if (!p)
-    return nullptr;
-
+  auto* p = new FXBMP_Context;
+  p->bmp_ptr = pDecomp;
+  p->parent_ptr = this;
+  p->delegate_ptr = pDelegate;
   p->m_AllocFunc = bmp_alloc_func;
   p->m_FreeFunc = bmp_free_func;
-  p->bmp_ptr = nullptr;
-  p->parent_ptr = this;
-  p->bmp_ptr = bmp_create_decompress();
-  if (!p->bmp_ptr) {
-    FX_Free(p);
-    return nullptr;
-  }
+  memset(p->m_szLastError, 0, sizeof(p->m_szLastError));
+
   p->bmp_ptr->context_ptr = p;
-  p->bmp_ptr->err_ptr = m_szLastError;
+  p->bmp_ptr->err_ptr = p->m_szLastError;
   p->bmp_ptr->bmp_error_fn = bmp_error_data;
   p->bmp_ptr->bmp_get_row_fn = bmp_read_scanline;
   p->bmp_ptr->bmp_get_data_position_fn = bmp_get_data_position;
@@ -80,9 +77,10 @@ FXBMP_Context* CCodec_BmpModule::Start() {
 void CCodec_BmpModule::Finish(FXBMP_Context* ctx) {
   if (ctx) {
     bmp_destroy_decompress(&ctx->bmp_ptr);
-    ctx->m_FreeFunc(ctx);
+    delete ctx;
   }
 }
+
 int32_t CCodec_BmpModule::ReadHeader(FXBMP_Context* ctx,
                                      int32_t* width,
                                      int32_t* height,
