a89aefbd38466e4754421595f9e19cf3f913305c
Diana Gage <drgage@google.com>
2017-06-08 18:41:55 -0700

    Method for obtaining selected text within a form field text field.
    1. Added public API for getting selected form field text and
    associated private functions all the way down to cpwl_edit_ctrl.cpp
    
    Change-Id: Ie77de38e45bbe6f9ea033826c961435304eedfc7

--- fpdfsdk/formfiller/cffl_formfiller.h
+++ fpdfsdk/formfiller/cffl_formfiller.h
@@ -73,6 +73,7 @@ class CFFL_FormFiller : public IPWL_Provider, public CPWL_TimerHandler {
                          uint32_t nKeyCode,
                          uint32_t nFlags);
   virtual bool OnChar(CPDFSDK_Annot* pAnnot, uint32_t nChar, uint32_t nFlags);
+  virtual CFX_WideString GetSelectedText(CPDFSDK_Annot* pAnnot);
 
   void SetFocusForAnnot(CPDFSDK_Annot* pAnnot, uint32_t nFlag);
   void KillFocusForAnnot(CPDFSDK_Annot* pAnnot, uint32_t nFlag);
