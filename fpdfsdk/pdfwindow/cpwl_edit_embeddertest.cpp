e70c0b58636ff9c5aa2fd8b4dd3304b7f8275f2c
Diana Gage <drgage@google.com>
2017-06-13 13:52:50 -0700

    Add FORM_GetSelectedText() function.
    This function copies the selected text from a form text field 
    into the buffer parameter and returns the length of the selected text
    string. When passed nullptr for buffer or buflen is less than the 
    length of the selected text, this function does not modify the buffer
    and only returns the selected text length. 
    
    Change-Id: Ie77de38e45bbe6f9ea033826c961435304eedfc7

--- fpdfsdk/pdfwindow/cpwl_edit_embeddertest.cpp
+++ fpdfsdk/pdfwindow/cpwl_edit_embeddertest.cpp
@@ -2,6 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// DELETE
+#include <iostream>
+
 #include "fpdfsdk/cba_annotiterator.h"
 #include "fpdfsdk/cpdfsdk_annot.h"
 #include "fpdfsdk/cpdfsdk_formfillenvironment.h"
@@ -11,51 +14,113 @@
 #include "testing/embedder_test.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-class CPWLEditEmbeddertest : public EmbedderTest {};
+class CPWLEditEmbeddertest : public EmbedderTest {
+ protected:
+  void CreateAndInitializeFormPDF() {
+    EXPECT_TRUE(OpenDocument("text_form.pdf"));
+    page_ = LoadPage(0);
+    ASSERT_TRUE(page_);
+
+    CPDFSDK_FormFillEnvironment* pFormFillEnv =
+        static_cast<CPDFSDK_FormFillEnvironment*>(form_handle());
+
+    {
+      CBA_AnnotIterator iter(pFormFillEnv->GetPageView(0),
+                             CPDF_Annot::Subtype::WIDGET);
+      pAnnot_ = iter.GetFirstAnnot();
+      CPDFSDK_Annot* pLastAnnot = iter.GetLastAnnot();
+      ASSERT_EQ(pAnnot_, pLastAnnot);
+      ASSERT_TRUE(pAnnot_);
+      ASSERT_EQ(CPDF_Annot::Subtype::WIDGET, pAnnot_->GetAnnotSubtype());
+    }
+
+    CFFL_InteractiveFormFiller* pInteractiveFormFiller =
+        pFormFillEnv->GetInteractiveFormFiller();
+    {
+      CPDFSDK_Annot::ObservedPtr pObserved(pAnnot_);
+      EXPECT_TRUE(pInteractiveFormFiller->OnSetFocus(&pObserved, 0));
+    }
+
+    pFormFiller_ = pInteractiveFormFiller->GetFormFiller(pAnnot_, false);
+    ASSERT_TRUE(pFormFiller_);
+
+    CPWL_Wnd* pWindow =
+        pFormFiller_->GetPDFWindow(pFormFillEnv->GetPageView(0), false);
+    ASSERT_TRUE(pWindow);
+    ASSERT_EQ(PWL_CLASSNAME_EDIT, pWindow->GetClassName());
+
+    pEdit_ = static_cast<CPWL_Edit*>(pWindow);
+  }
+
+  FPDF_PAGE GetPage() { return page_; }
+  CPWL_Edit* GetCPWL_Edit() { return pEdit_; }
+  CFFL_FormFiller* GetCFFL_FormFiller() { return pFormFiller_; }
+  CPDFSDK_Annot* GetCPDFSDK_Annot() { return pAnnot_; }
+
+ private:
+  FPDF_PAGE page_;
+  CPWL_Edit* pEdit_;
+  CFFL_FormFiller* pFormFiller_;
+  CPDFSDK_Annot* pAnnot_;
+};
 
 TEST_F(CPWLEditEmbeddertest, TypeText) {
-  EXPECT_TRUE(OpenDocument("text_form.pdf"));
-  FPDF_PAGE page = LoadPage(0);
-  ASSERT_TRUE(page);
+  CreateAndInitializeFormPDF();
 
-  CPDFSDK_FormFillEnvironment* pFormFillEnv =
-      static_cast<CPDFSDK_FormFillEnvironment*>(form_handle());
+  EXPECT_TRUE(GetCPWL_Edit()->GetText().IsEmpty());
+  EXPECT_TRUE(GetCFFL_FormFiller()->OnChar(GetCPDFSDK_Annot(), 'a', 0));
+  EXPECT_TRUE(GetCFFL_FormFiller()->OnChar(GetCPDFSDK_Annot(), 'b', 0));
+  EXPECT_TRUE(GetCFFL_FormFiller()->OnChar(GetCPDFSDK_Annot(), 'c', 0));
 
-  CPDFSDK_Annot* pAnnot;
-  {
-    CBA_AnnotIterator iter(pFormFillEnv->GetPageView(0),
-                           CPDF_Annot::Subtype::WIDGET);
-    pAnnot = iter.GetFirstAnnot();
-    CPDFSDK_Annot* pLastAnnot = iter.GetLastAnnot();
-    ASSERT_EQ(pAnnot, pLastAnnot);
-    ASSERT_TRUE(pAnnot);
-    ASSERT_EQ(CPDF_Annot::Subtype::WIDGET, pAnnot->GetAnnotSubtype());
+  EXPECT_STREQ(L"abc", GetCPWL_Edit()->GetText().c_str());
+
+  UnloadPage(GetPage());
+}
+
+TEST_F(CPWLEditEmbeddertest, GetSelectedTextEmptyAndBasic) {
+  CreateAndInitializeFormPDF();
+
+  GetCPWL_Edit()->SetSel(0, 3);
+  EXPECT_TRUE(GetCPWL_Edit()->GetSelectedText().IsEmpty());
+
+  EXPECT_TRUE(GetCFFL_FormFiller()->OnChar(GetCPDFSDK_Annot(), 'a', 0));
+  EXPECT_TRUE(GetCFFL_FormFiller()->OnChar(GetCPDFSDK_Annot(), 'b', 0));
+  EXPECT_TRUE(GetCFFL_FormFiller()->OnChar(GetCPDFSDK_Annot(), 'c', 0));
+  GetCPWL_Edit()->SetSel(0, 2);
+
+  EXPECT_STREQ(L"ab", GetCPWL_Edit()->GetSelectedText().c_str());
+
+  UnloadPage(GetPage());
+}
+
+TEST_F(CPWLEditEmbeddertest, GetSelectedTextFragments) {
+  CreateAndInitializeFormPDF();
+
+  for (int i = 0; i < 50; ++i) {
+    EXPECT_TRUE(GetCFFL_FormFiller()->OnChar(GetCPDFSDK_Annot(), i + 'A', 0));
   }
 
-  CFFL_InteractiveFormFiller* pInteractiveFormFiller =
-      pFormFillEnv->GetInteractiveFormFiller();
-  {
-    CPDFSDK_Annot::ObservedPtr pObserved(pAnnot);
-    EXPECT_TRUE(pInteractiveFormFiller->OnSetFocus(&pObserved, 0));
-  }
+  GetCPWL_Edit()->SetSel(0, 0);
+  EXPECT_TRUE(GetCPWL_Edit()->GetSelectedText().IsEmpty());
 
-  CFFL_FormFiller* pFormFiller =
-      pInteractiveFormFiller->GetFormFiller(pAnnot, false);
-  ASSERT_TRUE(pFormFiller);
+  GetCPWL_Edit()->SetSel(0, 1);
+  EXPECT_STREQ(L"A", GetCPWL_Edit()->GetSelectedText().c_str());
 
-  CPWL_Wnd* pWindow =
-      pFormFiller->GetPDFWindow(pFormFillEnv->GetPageView(0), false);
-  ASSERT_TRUE(pWindow);
-  ASSERT_EQ(PWL_CLASSNAME_EDIT, pWindow->GetClassName());
+  GetCPWL_Edit()->SetSel(0, -1);
+  EXPECT_STREQ(L"ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqr",
+               GetCPWL_Edit()->GetSelectedText().c_str());
 
-  CPWL_Edit* pEdit = static_cast<CPWL_Edit*>(pWindow);
-  EXPECT_TRUE(pEdit->GetText().IsEmpty());
+  GetCPWL_Edit()->SetSel(-8, -1);
+  EXPECT_TRUE(GetCPWL_Edit()->GetSelectedText().IsEmpty());
 
-  EXPECT_TRUE(pFormFiller->OnChar(pAnnot, 'a', 0));
-  EXPECT_TRUE(pFormFiller->OnChar(pAnnot, 'b', 0));
-  EXPECT_TRUE(pFormFiller->OnChar(pAnnot, 'c', 0));
+  GetCPWL_Edit()->SetSel(23, 12);
+  EXPECT_STREQ(L"MNOPQRSTUVW", GetCPWL_Edit()->GetSelectedText().c_str());
 
-  EXPECT_STREQ(L"abc", pEdit->GetText().c_str());
+  GetCPWL_Edit()->SetSel(12, 23);
+  EXPECT_STREQ(L"MNOPQRSTUVW", GetCPWL_Edit()->GetSelectedText().c_str());
 
-  UnloadPage(page);
+  GetCPWL_Edit()->SetSel(49, 50);
+  EXPECT_STREQ(L"r", GetCPWL_Edit()->GetSelectedText().c_str());
+
+  UnloadPage(GetPage());
 }
