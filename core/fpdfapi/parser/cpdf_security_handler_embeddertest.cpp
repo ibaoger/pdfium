08ddc02c7de45376e3d9ca366228f63608e990ce
Nicolas Pena <npm@chromium.org>
2017-06-27 14:32:10 -0400

    Add Embeddertest for password after saving
    Change-Id: Ie0da7f290711505fb208794afdc737c36e84dd3c

--- core/fpdfapi/parser/cpdf_security_handler_embeddertest.cpp
+++ core/fpdfapi/parser/cpdf_security_handler_embeddertest.cpp
@@ -2,10 +2,16 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <string>
+
+#include "public/fpdf_edit.h"
+#include "public/fpdf_save.h"
+#include "public/fpdfview.h"
 #include "testing/embedder_test.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-class CPDFSecurityHandlerEmbeddertest : public EmbedderTest {};
+class CPDFSecurityHandlerEmbeddertest : public EmbedderTest,
+                                        public TestSaver {};
 
 TEST_F(CPDFSecurityHandlerEmbeddertest, Unencrypted) {
   ASSERT_TRUE(OpenDocument("about_blank.pdf"));
@@ -33,6 +39,46 @@ TEST_F(CPDFSecurityHandlerEmbeddertest, UserPassword) {
 TEST_F(CPDFSecurityHandlerEmbeddertest, OwnerPassword) {
   ASSERT_TRUE(OpenDocument("encrypted.pdf", "5678"));
   EXPECT_EQ(0xFFFFFFFC, FPDF_GetDocPermissions(document()));
+}
+
+TEST_F(CPDFSecurityHandlerEmbeddertest, PasswordAfterGenerateSave) {
+  int num_page_objs = 0;
+  {
+    ASSERT_TRUE(OpenDocument("encrypted.pdf", "5678"));
+    FPDF_PAGE page = LoadPage(0);
+    ASSERT_TRUE(page);
+    FPDF_PAGEOBJECT path = FPDFPageObj_CreateNewPath(10.0f, 10.0f);
+    ASSERT_TRUE(path);
+    FPDFPath_MoveTo(path, 100.0f, 100.0f);
+    FPDFPage_InsertObject(page, path);
+    num_page_objs = FPDFPage_CountObject(page);
+    EXPECT_TRUE(FPDFPage_GenerateContent(page));
+    EXPECT_TRUE(FPDF_SaveAsCopy(document(), this, 0));
+    UnloadPage(page);
+  }
+  std::string new_file = GetString();
+  FPDF_FILEACCESS file_access;
+  memset(&file_access, 0, sizeof(file_access));
+  file_access.m_FileLen = new_file.size();
+  file_access.m_GetBlock = GetBlockFromString;
+  file_access.m_Param = &new_file;
+  EXPECT_FALSE(FPDF_LoadCustomDocument(&file_access, nullptr));
+  struct {
+    const char* password;
+    const unsigned long permissions;
+  } tests[] = {{"1234", 0xFFFFF2C0}, {"5678", 0xFFFFFFFC}};
+  for (const auto& test : tests) {
+    FPDF_DOCUMENT document =
+        FPDF_LoadCustomDocument(&file_access, test.password);
+    ASSERT_TRUE(document);
+    EXPECT_EQ(test.permissions, FPDF_GetDocPermissions(document));
+    EXPECT_EQ(1, FPDF_GetPageCount(document));
+    FPDF_PAGE page = FPDF_LoadPage(document, 0);
+    ASSERT_TRUE(page);
+    EXPECT_EQ(num_page_objs, FPDFPage_CountObject(page));
+    FPDF_ClosePage(page);
+    FPDF_CloseDocument(document);
+  }
 }
 
 TEST_F(CPDFSecurityHandlerEmbeddertest, NoPasswordVersion5) {
