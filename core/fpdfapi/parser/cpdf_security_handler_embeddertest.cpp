7055c9f59e5726db20b1ad9ec0266d00c7b429a9
Nicolas Pena <npm@chromium.org>
2017-07-13 14:18:43 -0400

    Add Embeddertest for password after saving
    Change-Id: Ie0da7f290711505fb208794afdc737c36e84dd3c

--- core/fpdfapi/parser/cpdf_security_handler_embeddertest.cpp
+++ core/fpdfapi/parser/cpdf_security_handler_embeddertest.cpp
@@ -2,6 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <string>
+
+#include "public/fpdf_edit.h"
+#include "public/fpdf_save.h"
+#include "public/fpdfview.h"
 #include "testing/embedder_test.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -33,6 +38,42 @@ TEST_F(CPDFSecurityHandlerEmbeddertest, UserPassword) {
 TEST_F(CPDFSecurityHandlerEmbeddertest, OwnerPassword) {
   ASSERT_TRUE(OpenDocument("encrypted.pdf", "5678"));
   EXPECT_EQ(0xFFFFFFFC, FPDF_GetDocPermissions(document()));
+}
+
+TEST_F(CPDFSecurityHandlerEmbeddertest, PasswordAfterGenerateSave) {
+  const char md5[] = "e4a3701ca5b2a759e06455aa8d97d46e";
+  {
+    ASSERT_TRUE(OpenDocument("encrypted.pdf", "5678", true));
+    FPDF_PAGE page = LoadPage(0);
+    ASSERT_TRUE(page);
+    FPDF_PAGEOBJECT red_rect = FPDFPageObj_CreateNewRect(10, 10, 20, 20);
+    ASSERT_TRUE(red_rect);
+    EXPECT_TRUE(FPDFPath_SetFillColor(red_rect, 255, 0, 0, 255));
+    EXPECT_TRUE(FPDFPath_SetDrawMode(red_rect, FPDF_FILLMODE_ALTERNATE, 0));
+    FPDFPage_InsertObject(page, red_rect);
+    FPDF_BITMAP page_bitmap = RenderPage(page);
+    CompareBitmap(page_bitmap, 612, 792, md5);
+    FPDFBitmap_Destroy(page_bitmap);
+    EXPECT_TRUE(FPDFPage_GenerateContent(page));
+    EXPECT_TRUE(FPDF_SaveAsCopy(document(), this, 0));
+    UnloadPage(page);
+  }
+  std::string new_file = GetString();
+  FPDF_FILEACCESS file_access;
+  memset(&file_access, 0, sizeof(file_access));
+  file_access.m_FileLen = new_file.size();
+  file_access.m_GetBlock = GetBlockFromString;
+  file_access.m_Param = &new_file;
+  EXPECT_FALSE(FPDF_LoadCustomDocument(&file_access, nullptr));
+  struct {
+    const char* password;
+    const unsigned long permissions;
+  } tests[] = {{"1234", 0xFFFFF2C0}, {"5678", 0xFFFFFFFC}};
+  for (const auto& test : tests) {
+    TestSaved(612, 792, md5, test.password);
+    EXPECT_EQ(test.permissions, FPDF_GetDocPermissions(m_SavedDocument));
+    CloseSaved();
+  }
 }
 
 TEST_F(CPDFSecurityHandlerEmbeddertest, NoPasswordVersion5) {
