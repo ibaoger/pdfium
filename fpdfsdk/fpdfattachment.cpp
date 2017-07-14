4ac8ffbf8db4cce0347a1b9387c223a83df89b34
Jane Liu <janeliulwq@google.com>
2017-07-14 11:39:47 -0400

    Basic APIs and tests for extracting attachments
    1. Added API for extracting attachment properties and data.
      * Expanded the embedder test to cover all the new APIs.
    
    Bug=pdfium:174
    
    Change-Id: I09bffd412410e9aea45faca442d2b72eefafef4e

--- fpdfsdk/fpdfattachment.cpp
+++ fpdfsdk/fpdfattachment.cpp
@@ -4,10 +4,41 @@
 
 #include "public/fpdf_attachment.h"
 
+#include "core/fpdfapi/page/cpdf_streamparser.h"
 #include "core/fpdfapi/parser/cpdf_document.h"
+#include "core/fpdfapi/parser/cpdf_string.h"
+#include "core/fpdfapi/parser/fpdf_parser_decode.h"
 #include "core/fpdfdoc/cpdf_filespec.h"
 #include "core/fpdfdoc/cpdf_nametree.h"
 #include "fpdfsdk/fsdk_define.h"
+
+namespace {
+
+CPDF_Object* GetEmbeddedFileObject(FPDF_DOCUMENT document, int index) {
+  CPDF_Document* pDoc = CPDFDocumentFromFPDFDocument(document);
+  if (!pDoc || index < 0)
+    return nullptr;
+
+  CPDF_NameTree nameTree(pDoc, "EmbeddedFiles");
+  if (static_cast<size_t>(index) >= nameTree.GetCount())
+    return nullptr;
+
+  CFX_ByteString csName;
+  return nameTree.LookupValueAndName(index, &csName);
+}
+
+unsigned long Utf16EncodeMaybeCopyAndReturnLength(const CFX_WideString& src,
+                                                  void* buffer,
+                                                  unsigned long buflen) {
+  CFX_ByteString text = src.UTF16LE_Encode();
+  unsigned long len = text.GetLength();
+  if (buffer && buflen >= len)
+    memcpy(buffer, text.c_str(), len);
+
+  return len;
+}
+
+}  // namespace
 
 DLLEXPORT int STDCALL FPDFDoc_GetAttachmentCount(FPDF_DOCUMENT document) {
   CPDF_Document* pDoc = CPDFDocumentFromFPDFDocument(document);
@@ -22,23 +53,96 @@ FPDFDoc_GetAttachmentName(FPDF_DOCUMENT document,
                           int index,
                           void* buffer,
                           unsigned long buflen) {
-  CPDF_Document* pDoc = CPDFDocumentFromFPDFDocument(document);
-  if (!pDoc || index < 0)
-    return 0;
-
-  CPDF_NameTree nameTree(pDoc, "EmbeddedFiles");
-  if (static_cast<size_t>(index) >= nameTree.GetCount())
-    return 0;
-
-  CFX_ByteString csName;
-  CPDF_Object* pFile = nameTree.LookupValueAndName(index, &csName);
+  CPDF_Object* pFile = GetEmbeddedFileObject(document, index);
   if (!pFile)
     return 0;
 
-  CFX_ByteString name = CPDF_FileSpec(pFile).GetFileName().UTF16LE_Encode();
-  unsigned long len = name.GetLength();
-  if (buffer && buflen >= len)
-    memcpy(buffer, name.c_str(), len);
+  return Utf16EncodeMaybeCopyAndReturnLength(CPDF_FileSpec(pFile).GetFileName(),
+                                             buffer, buflen);
+}
 
-  return len;
+DLLEXPORT unsigned long STDCALL
+FPDFDoc_GetAttachmentDate(FPDF_DOCUMENT document,
+                          int index,
+                          FPDFATTACHMENT_DATETYPE type,
+                          void* buffer,
+                          unsigned long buflen) {
+  CPDF_Object* pFile = GetEmbeddedFileObject(document, index);
+  if (!pFile)
+    return 0;
+
+  CPDF_Dictionary* pParamsDict = CPDF_FileSpec(pFile).GetParamsDict();
+  if (!pParamsDict)
+    return 0;
+
+  if (type != FPDFATTACHMENT_DATETYPE_Creation &&
+      type != FPDFATTACHMENT_DATETYPE_Modification) {
+    return 0;
+  }
+
+  CFX_WideString value = pParamsDict->GetUnicodeTextFor(
+      type == FPDFATTACHMENT_DATETYPE_Creation ? "CreationDate" : "ModDate");
+  return Utf16EncodeMaybeCopyAndReturnLength(value, buffer, buflen);
+}
+
+DLLEXPORT unsigned long STDCALL
+FPDFDoc_GetAttachmentCheckSum(FPDF_DOCUMENT document,
+                              int index,
+                              void* buffer,
+                              unsigned long buflen) {
+  CPDF_Object* pFile = GetEmbeddedFileObject(document, index);
+  if (!pFile)
+    return 0;
+
+  CPDF_FileSpec filespec(pFile);
+  CPDF_Dictionary* pParamsDict = filespec.GetParamsDict();
+  if (!pParamsDict)
+    return 0;
+
+  CFX_ByteString key = "CheckSum";
+  CPDF_String* stringValue = pParamsDict->GetObjectFor(key)->AsString();
+  CFX_WideString value =
+      stringValue->IsHex()
+          ? CPDF_String(nullptr,
+                        PDF_EncodeString(stringValue->GetString(), true), false)
+                .GetUnicodeText()
+          : pParamsDict->GetUnicodeTextFor(key);
+  return Utf16EncodeMaybeCopyAndReturnLength(value, buffer, buflen);
+}
+
+DLLEXPORT unsigned long STDCALL
+FPDFDoc_GetAttachmentFile(FPDF_DOCUMENT document,
+                          int index,
+                          void* buffer,
+                          unsigned long buflen) {
+  CPDF_Object* pFile = GetEmbeddedFileObject(document, index);
+  if (!pFile)
+    return 0;
+
+  CPDF_Stream* pFileStream = CPDF_FileSpec(pFile).GetFileStream();
+  if (!pFileStream)
+    return 0;
+
+  uint8_t* data = pFileStream->GetRawData();
+  uint32_t len = pFileStream->GetRawSize();
+  CPDF_Dictionary* pFileDict = pFileStream->GetDict();
+  if (!pFileDict || pFileDict->GetStringFor("Filter").IsEmpty()) {
+    if (buffer && buflen >= len)
+      memcpy(buffer, data, len);
+
+    return len;
+  }
+
+  // Decode the stream if a stream filter is specified.
+  uint8_t* decodedData = nullptr;
+  uint32_t decodedLen = 0;
+  CPDF_StreamParser::DecodeInlineStream(
+      data, len, pFileDict->GetIntegerFor("Width"),
+      pFileDict->GetIntegerFor("Height"), pFileDict->GetStringFor("Filter"),
+      pFileDict->GetDictFor("DecodeParms"), &decodedData, &decodedLen);
+  if (buffer && buflen >= decodedLen)
+    memcpy(buffer, decodedData, decodedLen);
+
+  FX_Free(decodedData);
+  return decodedLen;
 }
--- public/fpdf_attachment.h
+++ public/fpdf_attachment.h
@@ -12,6 +12,11 @@
 extern "C" {
 #endif  // __cplusplus
 
+typedef enum FPDFATTACHMENT_DATETYPE {
+  FPDFATTACHMENT_DATETYPE_Creation = 0,
+  FPDFATTACHMENT_DATETYPE_Modification,
+} FPDFATTACHMENT_DATETYPE;
+
 // Experimental API.
 // Get the number of embedded files in |document|.
 //
@@ -33,6 +38,62 @@ DLLEXPORT int STDCALL FPDFDoc_GetAttachmentCount(FPDF_DOCUMENT document);
 // Returns the length of the file name.
 DLLEXPORT unsigned long STDCALL
 FPDFDoc_GetAttachmentName(FPDF_DOCUMENT document,
+                          int index,
+                          void* buffer,
+                          unsigned long buflen);
+
+// Experimental API.
+// Get the date associated with the embedded file at |index| in |document|.
+// |buffer| is only modified if |buflen| is longer than the length of the file's
+// date string. On errors, |buffer| is unmodified and the returned
+// length is 0.
+//
+//   document - handle to a document.
+//   index    - the index of the requested embedded file.
+//   type     - type of the date requested.
+//   buffer   - buffer for holding the file's date string, encoded in UTF16-LE.
+//   buflen   - length of the buffer.
+//
+// Returns the length of the file's date string.
+DLLEXPORT unsigned long STDCALL
+FPDFDoc_GetAttachmentDate(FPDF_DOCUMENT document,
+                          int index,
+                          FPDFATTACHMENT_DATETYPE type,
+                          void* buffer,
+                          unsigned long buflen);
+
+// Experimental API.
+// Get the checksum string of the embedded file at |index| in |document|. If the
+// checksum is a hexadecimal string, the returned string is the original string
+// before decoding.  |buffer| is only modified if |buflen| is longer than the
+// length of the file's checksum string. On errors, |buffer| is unmodified
+// and the returned length is 0.
+//
+//   document - handle to a document.
+//   index    - the index of the requested embedded file.
+//   buffer   - buffer for holding the file's checksum, encoded in UTF16-LE.
+//   buflen   - length of the buffer.
+//
+// Returns the length of the file's checksum string.
+DLLEXPORT unsigned long STDCALL
+FPDFDoc_GetAttachmentCheckSum(FPDF_DOCUMENT document,
+                              int index,
+                              void* buffer,
+                              unsigned long buflen);
+
+// Experimental API.
+// Get the embedded file at |index| in |document|. |buffer| is only modified if
+// |buflen| is longer than the length of the file. On errors, |buffer| is
+// unmodified and the returned length is 0.
+//
+//   document - handle to a document.
+//   index    - the index of the requested embedded file.
+//   buffer   - buffer for holding the file's data in raw bytes.
+//   buflen   - length of the buffer.
+//
+// Returns the length of the file.
+DLLEXPORT unsigned long STDCALL
+FPDFDoc_GetAttachmentFile(FPDF_DOCUMENT document,
                           int index,
                           void* buffer,
                           unsigned long buflen);
