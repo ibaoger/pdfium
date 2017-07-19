e0643c4792bad85d5f8c0c0bfbf43c8560bd6546
Jane Liu <janeliulwq@google.com>
2017-07-19 19:38:51 -0400

    Added CPDF_NameTree::AddValueAndName()
    1. Added CPDF_NameTree::AddValueAndName() for inserting new name and
    values pairs into a nametree while following nametree order rules. This
    function will be used for the API for adding embedded files.
        * Added anonymous helper functions.
        * Added two unit tests.
    
    Bug=pdfium:174
    
    Change-Id: Ic3e2f32f7147ef5c164cf5c2a28e0a652ffed74a

--- core/fpdfdoc/cpdf_nametree.cpp
+++ core/fpdfdoc/cpdf_nametree.cpp
@@ -6,14 +6,119 @@
 
 #include "core/fpdfdoc/cpdf_nametree.h"
 
+#include <utility>
+#include <vector>
+
 #include "core/fpdfapi/parser/cpdf_array.h"
 #include "core/fpdfapi/parser/cpdf_dictionary.h"
 #include "core/fpdfapi/parser/cpdf_document.h"
+#include "core/fpdfapi/parser/cpdf_string.h"
 #include "core/fpdfapi/parser/fpdf_parser_decode.h"
 
 namespace {
 
 const int nMaxRecursion = 32;
+
+bool SearchAddNameNode(CPDF_Dictionary* pNode,
+                       const CFX_WideString& csName,
+                       int nLevel,
+                       CPDF_Array** ppFind,
+                       int* pFindIndex) {
+  if (nLevel > nMaxRecursion)
+    return false;
+
+  CPDF_Array* pLimits = pNode->GetArrayFor("Limits");
+  if (pLimits) {
+    CFX_WideString csLeft = pLimits->GetUnicodeTextAt(0);
+    // If the lower limit is greater than the higher limit, swap them.
+    if (csLeft.Compare(pLimits->GetUnicodeTextAt(1)) > 0)
+      csLeft = pLimits->GetUnicodeTextAt(1);
+
+    // Skip this node if the name to look for comes before this node.
+    if (csName.Compare(csLeft) < 0)
+      return true;
+  }
+
+  CPDF_Array* pNames = pNode->GetArrayFor("Names");
+  if (pNames) {
+    // Loop through the names array to compare with the name to be added.
+    for (size_t i = 0; i < pNames->GetCount(); i += 2) {
+      int32_t iCompare = pNames->GetUnicodeTextAt(i).Compare(csName);
+      // Fail if this name tree already contains this name.
+      if (iCompare == 0)
+        return false;
+
+      // Records the current position if the name has not been reached yet.
+      if (iCompare < 0) {
+        if (ppFind)
+          *ppFind = pNames;
+
+        if (pFindIndex)
+          *pFindIndex = i;
+
+        continue;
+      }
+      // Return if the name has passed.
+      return true;
+    }
+    return true;
+  }
+
+  CPDF_Array* pKids = pNode->GetArrayFor("Kids");
+  if (!pKids)
+    return true;
+
+  for (size_t i = 0; i < pKids->GetCount(); ++i) {
+    CPDF_Dictionary* pKid = pKids->GetDictAt(i);
+    if (!pKid)
+      continue;
+
+    // Search through the kids and propagate any error back up.
+    if (!SearchAddNameNode(pKid, csName, nLevel + 1, ppFind, pFindIndex))
+      return false;
+  }
+  return true;
+}
+
+bool GetNodeLimits(const CPDF_Dictionary* pNode,
+                   const CPDF_Array* pFind,
+                   const CFX_WideString& csName,
+                   int nLevel,
+                   std::vector<CPDF_Array*>* pLimits) {
+  if (nLevel > nMaxRecursion)
+    return false;
+
+  CPDF_Array* pCurLimits = pNode->GetArrayFor("Limits");
+  if (pCurLimits) {
+    CFX_WideString csLeft = pCurLimits->GetUnicodeTextAt(0);
+    if (csLeft.Compare(pCurLimits->GetUnicodeTextAt(1)) > 0)
+      csLeft = pCurLimits->GetUnicodeTextAt(1);
+
+    if (csName.Compare(csLeft) < 0)
+      return false;
+  }
+
+  if (pNode->GetArrayFor("Names") == pFind) {
+    pLimits->push_back(pNode->GetArrayFor("Limits"));
+    return true;
+  }
+
+  CPDF_Array* pKids = pNode->GetArrayFor("Kids");
+  if (!pKids)
+    return false;
+
+  for (size_t i = 0; i < pKids->GetCount(); ++i) {
+    CPDF_Dictionary* pKid = pKids->GetDictAt(i);
+    if (!pKid)
+      continue;
+
+    if (GetNodeLimits(pKid, pFind, csName, nLevel + 1, pLimits)) {
+      pLimits->push_back(pNode->GetArrayFor("Limits"));
+      return true;
+    }
+  }
+  return false;
+}
 
 CPDF_Object* SearchNameNode(CPDF_Dictionary* pNode,
                             const CFX_WideString& csName,
@@ -166,6 +271,47 @@ int CPDF_NameTree::GetIndex(const CFX_WideString& csName) const {
   if (!SearchNameNode(m_pRoot.Get(), csName, nIndex, nullptr))
     return -1;
   return nIndex;
+}
+
+CPDF_Object* CPDF_NameTree::AddValueAndName(const CFX_WideString& name,
+                                            std::unique_ptr<CPDF_Object> pObj) {
+  if (!m_pRoot)
+    return nullptr;
+
+  CPDF_Array* pFind = nullptr;
+  int index = -2;
+  // Fails if the name tree already contains this name, or if this name tree is
+  // too deep.
+  if (!SearchAddNameNode(m_pRoot.Get(), name, 0, &pFind, &index))
+    return nullptr;
+
+  // Finds the leftmost names array if the new name will be the leftmost leaf.
+  if (!pFind) {
+    size_t nCurIndex = 0;
+    CFX_WideString csName;
+    SearchNameNode(m_pRoot.Get(), 0, nCurIndex, &csName, &pFind);
+  }
+
+  // Insert the name and the object into the names array found.
+  ASSERT(pFind);
+  CPDF_Object* pRet = pObj.get();
+  pFind->InsertNewAt<CPDF_String>(index + 2, name);
+  pFind->InsertAt(index + 3, std::move(pObj));
+
+  // Expand the limits if needed.
+  std::vector<CPDF_Array*> pLimits;
+  GetNodeLimits(m_pRoot.Get(), pFind, name, 0, &pLimits);
+  for (auto pLimit : pLimits) {
+    if (!pLimit)
+      continue;
+
+    if (name.Compare(pLimit->GetUnicodeTextAt(0)) < 0)
+      pLimit->SetNewAt<CPDF_String>(0, name);
+
+    if (name.Compare(pLimit->GetUnicodeTextAt(1)) > 0)
+      pLimit->SetNewAt<CPDF_String>(1, name);
+  }
+  return pRet;
 }
 
 CPDF_Object* CPDF_NameTree::LookupValueAndName(int nIndex,
