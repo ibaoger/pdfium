34d5fa6dc1cb4beed4826d5ac5cc8cb724426291
Henrique Nakashima <hnakashima@chromium.org>
2017-08-03 11:42:26 -0400

    Extracting GetBooleanGnArg from coverage_report.py
    Change-Id: I5475b37aad69361e2e7fae4ce69faf011e3b5c40

--- testing/tools/common.py
+++ testing/tools/common.py
@@ -5,6 +5,7 @@
 
 import glob
 import os
+import re
 import subprocess
 import sys
 
@@ -96,3 +97,16 @@ class DirectoryFinder:
     if other_components:
       result = os.path.join(result, other_components)
     return result
+
+
+def GetBooleanGnArg(arg_name, build_dir, verbose=False):
+  '''Extract the value of a boolean flag in args.gn'''
+  cwd = os.getcwd()
+  os.chdir(build_dir)
+  gn_args_output = subprocess.check_output(
+      ['gn', 'args', '.', '--list=%s' % arg_name, '--short'])
+  os.chdir(cwd)
+  arg_match_output = re.search('%s = (.*)' % arg_name, gn_args_output).group(1)
+  if verbose:
+    print >> sys.stderr, "Found '%s' for value of %s" % (arg_match_output, arg)
+  return arg_match_output == 'true'
