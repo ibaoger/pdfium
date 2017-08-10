448a5991cfc3919c25903b06a4b777c0c986396b
Ryan Harrison <rharrison@chromium.org>
2017-08-10 16:44:17 -0400

    Remove potential out of bounds call to GetAt()
    Since m_pos is passed into GetAt() on the under lying string in
    getCurrentChar(), the value of it needs to confirmed to be valid after
    decrementing. Some types were changed to reflect the values being
    stored.
    
    BUG=chromium:752480
    
    Change-Id: Ib6d6f52326defd31785e70a17049a08b64dbe069

--- fxbarcode/datamatrix/BC_C40Encoder.cpp
+++ fxbarcode/datamatrix/BC_C40Encoder.cpp
@@ -189,6 +189,9 @@ int32_t CBC_C40Encoder::encodeChar(wchar_t c, CFX_WideString& sb, int32_t& e) {
 int32_t CBC_C40Encoder::BacktrackOneCharacter(CBC_EncoderContext* context,
                                               CFX_WideString* buffer,
                                               int32_t lastCharSize) {
+  if (context->m_pos < 1)
+    return -1;
+
   int32_t count = buffer->GetLength();
   buffer->Delete(count - lastCharSize, count);
   context->m_pos--;
@@ -199,7 +202,7 @@ int32_t CBC_C40Encoder::BacktrackOneCharacter(CBC_EncoderContext* context,
   if (e != BCExceptionNO)
     return -1;
 
-  assert(len > 0);
+  ASSERT(len > 0);
   context->resetSymbolInfo();
   return len;
 }
