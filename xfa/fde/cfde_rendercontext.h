a93928ef4b88aa2ab6f39a6f0451959ca7686602
Dan Sinclair <dsinclair@chromium.org>
2017-08-14 11:59:22 -0400

    Cleaning up the visual set iterator
    This CL starts cleaning up the visual set iterator code.
    
    Change-Id: I643dc9edeeb0c7ff06ccd34a1977f811675413d8

--- xfa/fde/cfde_rendercontext.h
+++ xfa/fde/cfde_rendercontext.h
@@ -7,26 +7,21 @@
 #ifndef XFA_FDE_CFDE_RENDERCONTEXT_H_
 #define XFA_FDE_CFDE_RENDERCONTEXT_H_
 
-#include <memory>
 #include <vector>
 
 #include "core/fxcrt/fx_coordinates.h"
-#include "xfa/fde/cfde_renderdevice.h"
-#include "xfa/fde/cfde_visualsetiterator.h"
-#include "xfa/fde/ifde_visualset.h"
+#include "xfa/fde/cfde_txtedtpage.h"
 
 class CFDE_RenderDevice;
 class CFDE_TxtEdtTextSet;
 
 class CFDE_RenderContext {
  public:
-  CFDE_RenderContext();
+  CFDE_RenderContext(CFDE_RenderDevice* pRenderDevice,
+                     const CFX_Matrix& tmDoc2Device);
   ~CFDE_RenderContext();
 
-  void StartRender(CFDE_RenderDevice* pRenderDevice,
-                   CFDE_TxtEdtPage* pCanvasSet,
-                   const CFX_Matrix& tmDoc2Device);
-  void DoRender();
+  void Render(CFDE_TxtEdtPage* pCanvasSet);
 
  private:
   void RenderText(CFDE_TxtEdtTextSet* pTextSet, FDE_TEXTEDITPIECE* pText);
@@ -34,7 +29,6 @@ class CFDE_RenderContext {
   CFDE_RenderDevice* m_pRenderDevice;
   CFX_Matrix m_Transform;
   std::vector<FXTEXT_CHARPOS> m_CharPos;
-  std::unique_ptr<CFDE_VisualSetIterator> m_pIterator;
 };
 
 #endif  // XFA_FDE_CFDE_RENDERCONTEXT_H_
