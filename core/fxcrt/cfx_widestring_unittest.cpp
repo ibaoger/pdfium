f17ffeb639181b020922500bc67bcbd404a5bca7
Ryan Harrison <rharrison@chromium.org>
2017-08-14 14:14:49 -0400

    Remove GetAt and SetAt from string classes
    These methods duplicate the behaviour of the [] operator and don't
    offer any additional safety. Folding them into the C++ standard
    operator.
    
    BUG=pdfium:860
    
    Change-Id: I3ef5e5e5a15376f040256b646eb0d90636e24b67

--- core/fxcrt/cfx_widestring_unittest.cpp
+++ core/fxcrt/cfx_widestring_unittest.cpp
@@ -10,26 +10,8 @@
 
 #include "testing/gtest/include/gtest/gtest.h"
 
-TEST(fxcrt, WideStringGetAt) {
-  CFX_WideString short_string(L"a");
-  CFX_WideString longer_string(L"abc");
-  CFX_WideString embedded_nul_string(L"ab\0c", 4);
-
-#ifndef NDEBUG
-  EXPECT_DEATH({ short_string.GetAt(-1); }, ".*");
-#endif
-  EXPECT_EQ(L'a', short_string.GetAt(0));
-#ifndef NDEBUG
-  EXPECT_DEATH({ short_string.GetAt(1); }, ".*");
-#endif
-  EXPECT_EQ(L'c', longer_string.GetAt(2));
-  EXPECT_EQ(L'b', embedded_nul_string.GetAt(1));
-  EXPECT_EQ(L'\0', embedded_nul_string.GetAt(2));
-  EXPECT_EQ(L'c', embedded_nul_string.GetAt(3));
-}
-
 TEST(fxcrt, WideStringOperatorSubscript) {
-  CFX_WideString abc(L"abc");
+  const CFX_WideString abc(L"abc");
 #ifndef NDEBUG
   EXPECT_DEATH({ abc[-1]; }, ".*");
 #endif
@@ -39,21 +21,28 @@ TEST(fxcrt, WideStringOperatorSubscript) {
 #ifndef NDEBUG
   EXPECT_DEATH({ abc[4]; }, ".*");
 #endif
-}
 
-TEST(fxcrt, WideStringSetAt) {
-  CFX_WideString abc(L"abc");
+  CFX_WideString mutable_abc = abc;
+  EXPECT_EQ(abc.c_str(), mutable_abc.c_str());
+  const wchar_t* c_str = abc.c_str();
 #ifndef NDEBUG
-  EXPECT_DEATH({ abc.SetAt(-1, L'd'); }, ".*");
+  EXPECT_DEATH({ mutable_abc[-1] = L'd'; }, ".*");
+  EXPECT_EQ(L"abc", abc);
 #endif
-  abc.SetAt(0, L'd');
-  EXPECT_EQ(L"dbc", abc);
-  abc.SetAt(1, L'e');
-  EXPECT_EQ(L"dec", abc);
-  abc.SetAt(2, L'f');
-  EXPECT_EQ(L"def", abc);
+  mutable_abc[0] = L'd';
+  EXPECT_EQ(c_str, abc.c_str());
+  EXPECT_NE(c_str, mutable_abc.c_str());
+  EXPECT_EQ(L"abc", abc);
+  EXPECT_EQ(L"dbc", mutable_abc);
+  mutable_abc[1] = L'e';
+  EXPECT_EQ(L"abc", abc);
+  EXPECT_EQ(L"dec", mutable_abc);
+  mutable_abc[2] = L'f';
+  EXPECT_EQ(L"abc", abc);
+  EXPECT_EQ(L"def", mutable_abc);
 #ifndef NDEBUG
-  EXPECT_DEATH({ abc.SetAt(3, L'g'); }, ".*");
+  EXPECT_DEATH({ mutable_abc[3] = L'g'; }, ".*");
+  EXPECT_EQ(L"abc", abc);
 #endif
 }
 
@@ -778,24 +767,6 @@ TEST(fxcrt, WideStringCFromVector) {
   CFX_WideStringC cleared_string(cleared_vec);
   EXPECT_EQ(0, cleared_string.GetLength());
   EXPECT_EQ(nullptr, cleared_string.raw_str());
-}
-
-TEST(fxcrt, WideStringCGetAt) {
-  CFX_WideStringC short_string(L"a");
-  CFX_WideStringC longer_string(L"abc");
-  CFX_WideStringC embedded_nul_string(L"ab\0c", 4);
-
-#ifndef NDEBUG
-  EXPECT_DEATH({ short_string.GetAt(-1); }, ".*");
-#endif
-  EXPECT_EQ(L'a', static_cast<wchar_t>(short_string.GetAt(0)));
-#ifndef NDEBUG
-  EXPECT_DEATH({ short_string.GetAt(1); }, ".*");
-#endif
-  EXPECT_EQ(L'c', static_cast<wchar_t>(longer_string.GetAt(2)));
-  EXPECT_EQ(L'b', static_cast<wchar_t>(embedded_nul_string.GetAt(1)));
-  EXPECT_EQ(L'\0', static_cast<wchar_t>(embedded_nul_string.GetAt(2)));
-  EXPECT_EQ(L'c', static_cast<wchar_t>(embedded_nul_string.GetAt(3)));
 }
 
 TEST(fxcrt, WideStringCOperatorSubscript) {
