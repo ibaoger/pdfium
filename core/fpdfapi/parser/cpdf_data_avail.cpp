8f905713edd3fa13d5999a86a4d5e3b0a5279b0e
Artem Strygin <art-snake@yandex-team.ru>
2017-08-28 20:29:02 +0300

    Fix load non first pages in linearized document with hints table.
    Tha problem was, that when we use hints tables, we do not check
    pages tree availability, but currently for receiving page object
    in CPDF_Document, the pages tree should be available anyway.
    
    Change-Id: I908d00027fd8727f074a38e47fea095229ef5147

--- core/fpdfapi/parser/cpdf_data_avail.cpp
+++ core/fpdfapi/parser/cpdf_data_avail.cpp
@@ -1355,8 +1355,10 @@ CPDF_DataAvail::DocAvailStatus CPDF_DataAvail::IsPageAvail(
       nResult = m_pHintTables->CheckPage(dwPage, pHints);
       if (nResult != DataAvailable)
         return nResult;
-      m_pagesLoadState.insert(dwPage);
-      return GetPage(dwPage) ? DataAvailable : DataError;
+      if (GetPage(dwPage)) {
+        m_pagesLoadState.insert(dwPage);
+        return DataAvailable;
+      }
     }
 
     if (!m_bMainXRefLoadedOK) {
