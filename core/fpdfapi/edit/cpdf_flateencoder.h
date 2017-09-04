cddd9dab9d98ad5f243fd92e024ea64fd1a1ea17
Artem Strygin <art-snake@yandex-team.ru>
2017-09-04 19:02:42 +0300

    Refactoring of cpdf_flateencoder.
    Use const object as parametr for encoding.
    
    Change-Id: I6d41b97c3f8debd3de14d2aa044d1443cdc363ec

--- core/fpdfapi/edit/cpdf_flateencoder.h
+++ core/fpdfapi/edit/cpdf_flateencoder.h
@@ -7,6 +7,10 @@
 #ifndef CORE_FPDFAPI_EDIT_CPDF_FLATEENCODER_H_
 #define CORE_FPDFAPI_EDIT_CPDF_FLATEENCODER_H_
 
+#include <memory>
+#include <utility>
+#include <vector>
+
 #include "core/fpdfapi/parser/cpdf_dictionary.h"
 #include "core/fpdfapi/parser/cpdf_stream_acc.h"
 #include "core/fxcrt/cfx_maybe_owned.h"
@@ -17,21 +21,24 @@ class CPDF_Stream;
 
 class CPDF_FlateEncoder {
  public:
-  CPDF_FlateEncoder(CPDF_Stream* pStream, bool bFlateEncode);
+  CPDF_FlateEncoder(const CPDF_Stream* pStream, bool bFlateEncode);
   ~CPDF_FlateEncoder();
-
-  void CloneDict();
 
   uint32_t GetSize() const { return m_dwSize; }
   uint8_t* GetData() const { return m_pData.Get(); }
 
-  CPDF_Dictionary* GetDict() { return m_pDict.Get(); }
+  std::vector<std::pair<const CFX_ByteStringC, std::unique_ptr<CPDF_Object>>>&
+  changed_values() {
+    return m_ChangedValues;
+  }
 
  private:
   uint32_t m_dwSize;
   CFX_MaybeOwned<uint8_t, FxFreeDeleter> m_pData;
-  CFX_MaybeOwned<CPDF_Dictionary> m_pDict;
   CFX_RetainPtr<CPDF_StreamAcc> m_pAcc;
+  std::vector<std::pair<const CFX_ByteStringC, std::unique_ptr<CPDF_Object>>>
+      m_ChangedValues;
+  std::vector<std::unique_ptr<CPDF_Object>> m_ObjectsStore;
 };
 
 #endif  // CORE_FPDFAPI_EDIT_CPDF_FLATEENCODER_H_
