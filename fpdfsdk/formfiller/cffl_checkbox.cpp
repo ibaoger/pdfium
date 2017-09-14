02eed34489453f558f7bffe1a01ea5b4da6e5e03
Claudio DeSouza <claudiomdsjr@gmail.com>
2017-09-14 17:26:07 +0100

    Checkbox event fixed when triggered by a keyboard entry
    The event was changed to behave in the same way as when reacting to a mouse click event
    
    R=brucedawson@chromium.org, dsinclair@chromium.org, jam@chromium.org, npm@chromium.org, thakis@chromium.org, thestig@chromium.org, tsepez@chromium.org, weili@chromium.org
    
    Bug: 897
    Change-Id: I14366d69ad309e00cb1dd78672870d1ee95007a9

--- fpdfsdk/formfiller/cffl_checkbox.cpp
+++ fpdfsdk/formfiller/cffl_checkbox.cpp
@@ -58,9 +58,10 @@ bool CFFL_CheckBox::OnChar(CPDFSDK_Annot* pAnnot,
       }
 
       CFFL_FormFiller::OnChar(pAnnot, nChar, nFlags);
+      CPDFSDK_Widget* pWidget = static_cast<CPDFSDK_Widget*>(pAnnot);
       CPWL_CheckBox* pWnd = GetCheckBox(pPageView, true);
       if (pWnd)
-        pWnd->SetCheck(!pWnd->IsChecked());
+        pWnd->SetCheck(!pWidget->IsChecked());
 
       return CommitData(pPageView, nFlags);
     }
