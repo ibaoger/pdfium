489bd0d59274e3bd070f37eef06cecc5c35911f5
Artem Strygin <art-snake@yandex-team.ru>
2017-10-03 18:20:00 +0300

    Move the CryptoHandler into the SecurityHandler
    Change-Id: Idb5928e65833641d0443d955e4f2866d0f94cf5f

--- core/fpdfapi/parser/cpdf_parser.cpp
+++ core/fpdfapi/parser/cpdf_parser.cpp
@@ -156,7 +156,7 @@ void CPDF_Parser::SetEncryptDictionary(CPDF_Dictionary* pDict) {
 }
 
 CPDF_CryptoHandler* CPDF_Parser::GetCryptoHandler() const {
-  return m_pCryptoHandler.get();
+  return m_pSecurityHandler ? m_pSecurityHandler->GetCryptoHandler() : nullptr;
 }
 
 RetainPtr<IFX_SeekableReadStream> CPDF_Parser::GetFileAccess() const {
@@ -315,17 +315,15 @@ CPDF_Parser::Error CPDF_Parser::SetEncryptHandler() {
                                   m_Password))
       return PASSWORD_ERROR;
 
-    m_pSecurityHandler = std::move(pSecurityHandler);
-    auto pCryptoHandler = pdfium::MakeUnique<CPDF_CryptoHandler>();
-    if (!pCryptoHandler->Init(m_pEncryptDict.Get(), m_pSecurityHandler.get()))
+    if (!pSecurityHandler->InitCryptoHandler())
       return HANDLER_ERROR;
-    m_pCryptoHandler = std::move(pCryptoHandler);
+
+    m_pSecurityHandler = std::move(pSecurityHandler);
   }
   return SUCCESS;
 }
 
 void CPDF_Parser::ReleaseEncryptHandler() {
-  m_pCryptoHandler.reset();
   m_pSecurityHandler.reset();
   SetEncryptDictionary(nullptr);
 }
@@ -1276,9 +1274,9 @@ std::unique_ptr<CPDF_Object> CPDF_Parser::ParseIndirectObjectAtInternal(
   if (result && objnum && result->GetObjNum() != objnum)
     return nullptr;
 
-  const bool should_decrypt = m_pCryptoHandler && objnum != m_MetadataObjnum;
+  const bool should_decrypt = GetCryptoHandler() && objnum != m_MetadataObjnum;
   if (should_decrypt)
-    result = m_pCryptoHandler->DecryptObjectTree(std::move(result));
+    result = GetCryptoHandler()->DecryptObjectTree(std::move(result));
 
   return result;
 }
