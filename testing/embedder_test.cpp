339be7b9e98543e7b16e4474e5f3eae573a81e23
Dan Sinclair <dsinclair@chromium.org>
2017-10-18 10:29:15 -0400

    Fix rounding of colour values
    This CL fixes rounding issues with the colour values when written then
    read from path objects.
    
    Bug: pdfium:919
    Change-Id: I8ab33706f1c7d81c3ec755706b1a613cf2a557b3

--- testing/embedder_test.cpp
+++ testing/embedder_test.cpp
@@ -312,10 +312,7 @@ void EmbedderTest::UnloadPage(FPDF_PAGE page) {
   page_reverse_map_.erase(it);
 }
 
-void EmbedderTest::TestSaved(int width,
-                             int height,
-                             const char* md5,
-                             const char* password) {
+FPDF_DOCUMENT EmbedderTest::OpenSaved(const char* password) {
   memset(&saved_file_access_, 0, sizeof(saved_file_access_));
   saved_file_access_.m_FileLen = m_String.size();
   saved_file_access_.m_GetBlock = GetBlockFromString;
@@ -324,9 +321,18 @@ void EmbedderTest::TestSaved(int width,
   saved_fake_file_access_ =
       pdfium::MakeUnique<FakeFileAccess>(&saved_file_access_);
 
-  ASSERT_TRUE(OpenDocumentHelper(password, false, saved_fake_file_access_.get(),
+  EXPECT_TRUE(OpenDocumentHelper(password, false, saved_fake_file_access_.get(),
                                  &m_SavedDocument, &m_SavedAvail,
                                  &m_SavedForm));
+  return m_SavedDocument;
+}
+
+void EmbedderTest::TestSaved(int width,
+                             int height,
+                             const char* md5,
+                             const char* password) {
+  OpenSaved(password);
+
   EXPECT_EQ(1, FPDF_GetPageCount(m_SavedDocument));
   m_SavedPage = FPDF_LoadPage(m_SavedDocument, 0);
   ASSERT_TRUE(m_SavedPage);
