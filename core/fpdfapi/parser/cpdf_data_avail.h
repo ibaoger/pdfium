8119d42ed0b001d4483e16209cb26c27f5914d91
Artem Strygin <art-snake@yandex-team.ru>
2017-11-03 01:47:11 +0300

    Simplify check root availability.
    Change-Id: I34403490672b752bed9d5b6b29e8f4422c0fbf95

--- core/fpdfapi/parser/cpdf_data_avail.h
+++ core/fpdfapi/parser/cpdf_data_avail.h
@@ -166,8 +166,7 @@ class CPDF_DataAvail final {
 
   RetainPtr<CPDF_ReadValidator> m_pFileRead;
   CPDF_Parser m_parser;
-  std::unique_ptr<CPDF_Object> m_pRoot;
-  uint32_t m_dwRootObjNum;
+  std::unique_ptr<CPDF_Dictionary> m_pRoot;
   std::unique_ptr<CPDF_LinearizedHeader> m_pLinearized;
   bool m_bDocAvail;
   std::unique_ptr<CPDF_CrossRefAvail> m_pCrossRefAvail;
