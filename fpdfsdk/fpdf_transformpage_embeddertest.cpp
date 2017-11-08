3baa7b67fbaec00c51937db2e5bbbf99151b38b1
Shirleen Lou <xlou@chromium.org>
2017-11-08 15:15:25 -0800

    Add checks for matrix and clipRect in function FPDFPage_TransFormWithClip.  If both are null, abort transform. If either is null, skip that particular transaction. 	modified:   fpdfsdk/fpdf_transformpage.cpp
    Bug:
    Change-Id: I283746718fae2b38d62afcf2e7f213ab8cd90964

---
+++ fpdfsdk/fpdf_transformpage_embeddertest.cpp
@@ -0,0 +1,43 @@
+// Copyright 2017 PDFium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "public/fpdf_transformpage.h"
+#include "public/fpdfview.h"
+#include "testing/embedder_test.h"
+
+class FPDFTransformPageEmbeddertest : public EmbedderTest {};
+
+// TODO(xlou): Add more unit tests to increase function coverage.
+
+TEST_F(FPDFTransformPageEmbeddertest, FPDFTransFromPageWithNullptr) {
+  ASSERT_TRUE(OpenDocument("rectangles.pdf"));
+  FPDF_PAGE page = LoadPage(0);
+  EXPECT_NE(nullptr, page);
+  EXPECT_EQ(0, FPDFPage_TransFormWithClip(page, nullptr, nullptr));
+  UnloadPage(page);
+}
+
+TEST_F(FPDFTransformPageEmbeddertest, FPDF_TransFormPageWithClip) {
+  const char kRotatedMD5[] = "0a90de37f52127619c3dfb642b5fa2fe";
+  ASSERT_TRUE(OpenDocument("rectangles.pdf"));
+  FPDF_PAGE page = LoadPage(0);
+  EXPECT_NE(nullptr, page);
+  const int initial_width = static_cast<int>(FPDF_GetPageWidth(page));
+  const int initial_height = static_cast<int>(FPDF_GetPageHeight(page));
+
+  const float scale_factor = 0.5;
+  const float offset_x = 1.0;
+  const float offset_y = 1.0;
+  FS_MATRIX matrix = {scale_factor, 0, 0, scale_factor, offset_x, offset_y};
+  FS_RECTF cliprect = {offset_x, offset_y, offset_x, offset_y};
+  EXPECT_EQ(1, FPDFPage_TransFormWithClip(page, &matrix, &cliprect));
+  EXPECT_EQ(initial_width, static_cast<int>(FPDF_GetPageWidth(page)));
+  EXPECT_EQ(initial_height, static_cast<int>(FPDF_GetPageHeight(page)));
+
+  FPDF_BITMAP bitmap = RenderPage(page);
+  CompareBitmap(bitmap, initial_width, initial_height, kRotatedMD5);
+  FPDFBitmap_Destroy(bitmap);
+
+  UnloadPage(page);
+}
