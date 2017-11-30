2e04c2f2b229b7d3f0e3e03db36e6526310afccc
Ryan Harrison <rharrison@chromium.org>
2017-11-30 13:25:46 -0500

    Rewrite lower level details of extracting text from page
    The current implementation of text extraction was difficult to
    understand, duplicated logic that existed in other methods, and wasn't
    clear about the units the inputs were in. It also didn't handle
    control characters correctly.
    
    The new implementation leans on the methods for converting indices
    between the text buffer index and character list index spaces to avoid
    duplication of code. It also makes it clear to the reader that inputs
    are in the character list index space. Finally, it fixes issues being
    seen in Chrome with respect of ranges being slightly off.
    
    This CL also adds a test for extracting text that has control
    characters.
    
    BUG=pdfium:942,chromium:788811
    
    Change-Id: Id9d1f360c2d7492c7b5a48d6c9ae29f530892742

--- core/fpdftext/cpdf_textpage.h
+++ core/fpdftext/cpdf_textpage.h
@@ -103,7 +103,12 @@ class CPDF_TextPage {
   std::vector<CFX_FloatRect> GetRectArray(int start, int nCount) const;
   int GetIndexAtPos(const CFX_PointF& point, const CFX_SizeF& tolerance) const;
   WideString GetTextByRect(const CFX_FloatRect& rect) const;
-  WideString GetPageText(int start = 0, int nCount = -1) const;
+
+  // Returns string with the text from |m_TextBuf| that are covered by the input
+  // range. |start| and |count| are in terms of the m_CharIndex, so the range
+  // will be converted into appropriate indices. |count| == -1 indicates from
+  // |start| to the end of |m_TextBuf|.
+  WideString GetPageText(int start = 0, int count = -1) const;
   int CountRects(int start, int nCount);
   void GetRect(int rectIndex,
                float& left,
