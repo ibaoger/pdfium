252b44ac14d5d283b72966054613014b6afff04f
Dan Sinclair <dsinclair@chromium.org>
2017-12-14 10:02:06 -0500

    Fold CXFA_ScriptData into CXFA_Script
    This CL removes the CXFA_ScriptData wrapper and uses CXFA_Script
    directly.
    
    Change-Id: I1f1932ef96ee4a24407de7eb1d68b8631a5a67ea

--- xfa/fxfa/parser/cxfa_validate.cpp
+++ xfa/fxfa/parser/cxfa_validate.cpp
@@ -6,6 +6,8 @@
 
 #include "xfa/fxfa/parser/cxfa_validate.h"
 
+#include "xfa/fxfa/parser/cxfa_script.h"
+
 namespace {
 
 const CXFA_Node::PropertyData kPropertyData[] = {{XFA_Element::Message, 1, 0},
@@ -140,6 +142,6 @@ WideString CXFA_Validate::GetPicture() {
   return pNode ? pNode->JSObject()->GetContent(false) : L"";
 }
 
-CXFA_ScriptData CXFA_Validate::GetScriptData() {
-  return CXFA_ScriptData(GetChild(0, XFA_Element::Script, false));
+CXFA_Script* CXFA_Validate::GetScript() {
+  return static_cast<CXFA_Script*>(GetChild(0, XFA_Element::Script, false));
 }
