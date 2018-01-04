d92fb161a27a56ae7c9b3a28cf2e19002cadc56a
Ryan Harrison <rharrison@chromium.org>
2018-01-04 13:57:02 -0500

    Make CXFA_List::GetLength return a size_t
    This requires a number of other minor cleanups to get sizes and
    signedness agreeing everywhere.
    
    Change-Id: Ie25318a23d458be5a8a2a91927c3b37eb198f5ce

--- xfa/fxfa/parser/cxfa_node.h
+++ xfa/fxfa/parser/cxfa_node.h
@@ -153,7 +153,7 @@ class CXFA_Node : public CXFA_Object {
   CXFA_Node* GetModelNode();
   void UpdateNameHash();
 
-  int32_t CountChildren(XFA_Element eType, bool bOnlyChild);
+  size_t CountChildren(XFA_Element eType, bool bOnlyChild);
 
   template <typename T>
   T* GetChild(int32_t index, XFA_Element eType, bool bOnlyChild) {
