47b1954397bd275d381ada684f091016a7901eaf
Lei Zhang <thestig@chromium.org>
2018-01-09 00:30:46 -0800

    Move FreeType version check to its own method.
    Fix some other nits in CFX_FontMgr as well.
    
    Change-Id: I4c15d1cc96c964c452dcab8962885d2b71d54a5d

--- core/fxge/cfx_fontmgr.h
+++ core/fxge/cfx_fontmgr.h
@@ -60,6 +60,8 @@ class CFX_FontMgr {
   bool FTLibrarySupportsHinting() const { return m_FTLibrarySupportsHinting; }
 
  private:
+  bool FreeTypeVersionSupportsHinting() const;
+
   std::unique_ptr<CFX_FontMapper> m_pBuiltinMapper;
   std::map<ByteString, std::unique_ptr<CTTFontDesc>> m_FaceMap;
   FXFT_Library m_FTLibrary;
